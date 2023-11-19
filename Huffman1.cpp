c5c9dc60dfe48d4d4f67eaf0a1eab31b60a3eb8e
0Heitor <heitortanizaka@terra.com.br>
2023-11-19 14:52:37 -0300

    Lista Encadeada Ordenada

--- Huffman1.cpp
+++ Huffman1.cpp
@@ -8,13 +8,13 @@
 void salvarArquivoBinario(FILE **arq, Dados *dados){
 	Dados aux;
 	while(dados != NULL){
-		srtcpy(aux.cod,dados->cod);
+		strcpy(aux.cod,dados->cod);
 		aux.freq = dados->freq;
 		aux.simb = dados->simb;
 		strcpy(aux.palavra,dados->palavra);
 		aux.prox = NULL;
 		fwrite(&aux,sizeof(aux),1,*arq);
-		dados = dados->prox
+		dados = dados->prox;
 	}
 }
 
@@ -30,14 +30,54 @@ char verificaPalavra(Dados **dados,char palavra[]){
 	return 0;
 }
 
+int Quantidade(Dados *dados){
+	Dados *aux = dados;
+	int count=0;
+	while(aux != NULL){
+		count ++;
+		aux = aux->prox;
+	}
+	return count;
+}
+
 void ordenaLista(Dados **dados){
-	Dados *i = *dados,*j;
-	
-	for(i = *dados; i != NULL; i = i->prox){
-		for(j = *dados; j != NULL; j = j->prox){
-			
-		}		
-	}	
+	Dados *atual = *dados, *aux;
+	int x, count = Quantidade(*dados), freq,simb;
+	char cod[60],palavra[60];
+	for(x=0; x < count-1 ;x++){
+		aux = atual->prox;
+		while(aux!=NULL){
+			if(atual->freq > aux->freq ){
+				//Copiei os conteudos do atual para as variaveis
+				freq = atual->freq;
+				simb = atual->simb;
+				strcpy(cod,atual->cod);
+				strcpy(palavra,atual->palavra);
+				
+				//Copiei os conteudos do Aux para o Atual
+				atual->freq = aux->freq;
+				atual->simb = aux->simb;
+				strcpy(atual->cod,aux->cod);
+				strcpy(atual->palavra,aux->palavra);
+				
+				//Copiei os conteudos das variÃ¡veis para o Aux
+				aux->freq = freq;
+				aux->simb = simb;
+				strcpy(aux->cod,cod);
+				strcpy(aux->palavra,palavra);
+			}
+			aux = aux->prox;
+		}
+		atual = atual->prox;
+	}
+}
+
+void ExibeConteudoListaEncadeada(Dados *dados){
+	Dados *aux = dados;
+	while(aux != NULL){
+		printf("%s\t%d\t%d\t%s\n",aux->palavra,aux->freq,aux->simb,aux->cod);	
+		aux = aux->prox;
+	}
 }
 
 void pegarFrequencia(Dados **dados,char frase[]){
@@ -68,6 +108,8 @@ void pegarFrequencia(Dados **dados,char frase[]){
 			}
 		}
 	}
+	ExibeConteudoListaEncadeada(*dados);
+	printf("\n\n");
 	ordenaLista(&(*dados));
 }
 
@@ -84,19 +126,16 @@ void exibeH(Tree *tree){
 	}
 }
 
-int main(){
+int main(void){
 	setlocale(LC_ALL,"Portuguese");
 	Dados *dados,*aux;
 	Lista *lista;
 	char frase[500];
-	strcpy(frase,"Sem sacar que o espinho é seco. Sem sacar que seco é ser sol. Sem sacar que algum espinho seco secará. Se acabar não acostumando. Se acabar parado calado. Se acabar baixinho chorando. Se acabar meio abandonado.");
+	strcpy(frase,"Sem sacar que o espinho Ã© seco. Sem sacar que seco Ã© ser sol. Sem sacar que algum espinho seco secarÃ¡. Se acabar nÃ£o acostumando. Se acabar parado calado. Se acabar baixinho chorando. Se acabar meio abandonado.");
 	initDados(&dados);
 	initLista(&lista);
 	pegarFrequencia(&dados,frase);
-	aux = dados;
-	while(aux != NULL){
-		printf("%s\t%d\t%d\t%s\n",aux->palavra,aux->freq,aux->simb,aux->cod);	
-		aux = aux->prox;
-	}
-	
+	printf("\n\n\n");
+	ExibeConteudoListaEncadeada(dados);
+	return 0;
 }
